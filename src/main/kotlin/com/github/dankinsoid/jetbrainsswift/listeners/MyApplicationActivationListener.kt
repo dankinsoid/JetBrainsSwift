46ee3144914ef9eeb128f2e0351c13c9672d2763
GitHub Action <action@github.com>
2023-11-24 14:44:02 +0000

    Template cleanup

--- src/main/kotlin/com/github/dankinsoid/jetbrainsswift/listeners/MyApplicationActivationListener.kt
+++ src/main/kotlin/com/github/dankinsoid/jetbrainsswift/listeners/MyApplicationActivationListener.kt
@@ -1,4 +1,4 @@
-package org.jetbrains.plugins.template.listeners
+package com.github.dankinsoid.jetbrainsswift.listeners
 
 import com.intellij.openapi.application.ApplicationActivationListener
 import com.intellij.openapi.diagnostic.thisLogger
--- src/main/kotlin/com/github/dankinsoid/jetbrainsswift/services/MyProjectService.kt
+++ src/main/kotlin/com/github/dankinsoid/jetbrainsswift/services/MyProjectService.kt
@@ -1,9 +1,9 @@
-package org.jetbrains.plugins.template.services
+package com.github.dankinsoid.jetbrainsswift.services
 
 import com.intellij.openapi.components.Service
 import com.intellij.openapi.diagnostic.thisLogger
 import com.intellij.openapi.project.Project
-import org.jetbrains.plugins.template.MyBundle
+import com.github.dankinsoid.jetbrainsswift.MyBundle
 
 @Service(Service.Level.PROJECT)
 class MyProjectService(project: Project) {
